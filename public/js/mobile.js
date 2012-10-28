0eac9315420c62001f96737ad8c6c9b5d562f51b
Swizec Teller <swizec@swizec.com>
2012-10-29 00:12:43 +0100

    routing skeleton

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -1,4 +1,51 @@
 
+var AppRouter = Backbone.Router.extend({
+    
+    routes: {
+        "": "events",
+        "ready": "ready",
+        "waiting/:event": "waiting",
+        "handshake/:event": "handshake",
+        "melt/:event/:person": "melt",
+        "thanks": "thanks"
+    },
+
+    events: function () {
+        console.log("events");
+    },
+
+    ready: function () {
+        console.log("ready");
+    },
+
+    waiting: function (event) {
+        console.log("waiting");
+    },
+
+    handshake: function (event) {
+        console.log("handshake");
+    },
+
+    melt: function (event, person) {
+        console.log("melt");
+    },
+
+    thanks: function () {
+        console.log("thanks");
+    }
+    
+});
+
 (function ($) {
+
+    var app = new AppRouter();
+
+    Backbone.history.start({pushState: true});
+
+    $("a.btn").click(function (event) {
+        event.preventDefault();
+        app.navigate($(event.target).attr("href"),
+                    {trigger: true});
+    });
 
 })(jQuery);
