3333cae5d27e592f0f2ee6651b721092fa082ba9
Matej Rifelj <matej@rifelj.com>
2012-10-28 17:09:43 +0100

    Refactoring and a few lines of API

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
-var express = require('express'),
-    http = require('http'),
-    lessMiddleware = require('less-middleware'),
-    settings = require('./settings'),
-    routes = require('./routes');
+//-------------------------------------------------------//
+// Dependencies
+//-------------------------------------------------------//
+var express = require('express');
+var http = require('http');
+var lessMiddleware = require('less-middleware');
+var path = require('path');
+var _ = require('underscore')._;
+var step = require('step');
+var mongoose = require('mongoose');
+var db = mongoose.createConnection('localhost', 'meltee');
+var fs = require('fs');
 
+//-------------------------------------------------------//
+// Route dependencies
+//-------------------------------------------------------//
+var route_auth = require('./routes/auth');
+var route_topics = require('./routes/topics');
+var route_API = require('./routes/API');
 
-//------------------------------------------------------------------------------------------------------//
-// WEB SERVER
-//------------------------------------------------------------------------------------------------------//
+//-------------------------------------------------------//
+// Load settings
+//-------------------------------------------------------//
+var settings = fs.existsSync('./settings.js')?require('./settings'):require('./def_settings.js');
+
+//-------------------------------------------------------//
+// Globals
+//-------------------------------------------------------//
+global._ = _;
+global.step = step;
+global.db = db;
+global.mongoose = mongoose;
+global.settings = settings;
+
+var models = require('./models');
+global.models = models;
+//---------------------------------------------------------------//
+// Expressjs
+//---------------------------------------------------------------//
 var app = express();
 
-
 app.configure(function(){
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.session({ secret: "whatnot1347" }));
-    app.use(lessMiddleware({src: __dirname+'/public', compress: true}));
-    app.use(express.static(__dirname + '/public'));
-    app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
-    app.set('view engine', 'jade');
-    app.set('views', __dirname + '/views');
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: "whatnot1347" }));
+  app.use(lessMiddleware({src: __dirname+'/public', compress: true}));
+  app.use(express['static'](path.join(__dirname, 'public')));
+  app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
+  app.set('view engine', 'jade');
+  app.set('views', __dirname + '/views');
 });
 
 app.configure('development', function(){
@@ -30,19 +58,33 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+//---------------------------------------------------------------//
+// Frontend routes (mobile mostly)
+//---------------------------------------------------------------//
+app.get('/', route_auth.index);
+app.get('/auth', route_auth.auth);
+app.get('/access_token', route_auth.access_token);
+app.get('/logout', route_auth.logout);
+app.get('/topics.js', route_topics.get_topics);
+app.post('/save_topics', route_topics.save_topics);
 
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+//---------------------------------------------------------------//
+// Backend routes (admin CRUD)
+//---------------------------------------------------------------//
+app.get('/admin', function(req, res) { res.render('admin.jade'); });
 
-app.listen(settings.port);
+var route_API = require('./routes/API');
+//---------------------------------------------------------------//
+// API (crud)
+//---------------------------------------------------------------//
+app.post('/api/:collection', route_API.create);
+app.get('/api/:collection', route_API.read);
+app.put('/api/:collection/:id', route_API.update);
+app.delete('/api/:collection/:id', route_API.remove);
 
-app.get('/', routes.index);
-app.get('/auth', routes.auth);
-app.get('/access_token', routes.access_token);
-app.get('/logout', routes.logout);
-
-app.post('/save_topics', routes.data.save_topics);
-app.get('/topics.js', routes.data.topics);
-
-
-console.log("listening on http://localhost:"+settings.port);
+//------------------------------------------------------------------//
+// Create server
+//------------------------------------------------------------------//
+http.createServer(app).listen(settings.port, function(err){
+  console.log("Express server listening on port " + settings.port);
+});
