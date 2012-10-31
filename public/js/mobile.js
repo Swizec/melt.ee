250a1b75a8267d804d0ed6ec10607db34a3879f6
Swizec Teller <swizec@swizec.com>
2012-10-31 15:37:06 +0100

    getting to waiting screen works

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -1,105 +1,112 @@
 
-var Router = Backbone.Router.extend({
-    
-    routes: {
-        "mobile": "events",
-        "mobile/topics": "topics",
-        "mobile/ready/:event": "ready",
-        "mobile/waiting/:event": "waiting",
-        "mobile/handshake/:event": "handshake",
-        "mobile/melt/:event/:person": "melt",
-        "mobile/thanks": "thanks"
-    }
-});
-
-var PageView = Backbone.View.extend({
-    
-    events: {
-        "click a.btn": "__navigate"
-    },
-
-    render: function () {
-        this.$el.html(this.template());
-        return this.$el;
-    },
-
-    __navigate: function (event, href) {
-        event.preventDefault();
-        this.options.router.navigate('mobile'+href || 'mobile'+$(event.target).attr("href"),
-                                     {trigger: true});
-    }
-
-});
-
-var EventsView = PageView.extend({
-    template: Handlebars.compile($("#template-events").html()),
-
-    events: {
-        "click a.btn": "navigate"
-    },
-
-    navigate: function (event) {
-        var chosen = $(event.target).siblings("select").val();
-        this.__navigate(event, '/ready/'+chosen);
-    }
-});
-
-var TopicsView = PageView.extend({
-    template: Handlebars.compile($("#template-topics").html())
-
-});
-
-var ReadyView = PageView.extend({
-    template: Handlebars.compile($("#template-ready").html())
-});
-
-var WaitingView = PageView.extend({
-    template: Handlebars.compile($("#template-waiting").html())
-});
-
-var HandshakeView = PageView.extend({
-    template: Handlebars.compile($("#template-handshake").html())
-});
-
-var MeltView = PageView.extend({
-    template: Handlebars.compile($("#template-melt").html())
-});
-
-var ThanksView = PageView.extend({
-    template: Handlebars.compile($("#template-thanks").html())
-});
-
-
-var View = Backbone.View.extend({
-
-    pages: {"events": EventsView,
-            "topics": TopicsView,
-            "ready": ReadyView,
-            "waiting": WaitingView,
-            "handshake": HandshakeView, 
-            "melt": MeltView,
-            "thanks": ThanksView
-    },
-
-    initialize: function () {
-        _.keys(this.pages).map(function (route) {
-            this.options.router.on("route:"+route,
-                                   function () {
-                                       this.render(route);
-                                   }, this);
-        }, this);
-    },
-
-    render: function (route) {
-        var view = new this.pages[route]({el: this.$el,
-                                          router: this.options.router});
-
-        view.render();
-    }
-
-});
-
 (function ($) {
+
+    var Router = Backbone.Router.extend({
+        
+        routes: {
+            "mobile": "events",
+            "mobile/topics": "topics",
+            "mobile/ready/:event_id": "ready",
+            "mobile/waiting/:event_id": "waiting",
+            "mobile/handshake/:event_id": "handshake",
+            "mobile/melt/:event_id/:person": "melt",
+            "mobile/thanks": "thanks"
+        }
+    });
+
+    var PageView = Backbone.View.extend({
+        
+        events: {
+            "click a.btn": "__navigate"
+        },
+
+        render: function () {
+            this.$el.html(this.template(this.options));
+            return this.$el;
+        },
+
+        __navigate: function (event, href) {
+            event.preventDefault();
+
+            if (!href) {
+                href = $(event.target).attr("href");
+            }
+            
+            this.options.router.navigate('mobile'+href,
+                                         {trigger: true});
+        }
+
+    });
+
+    var EventsView = PageView.extend({
+        template: Handlebars.compile($("#template-events").html()),
+
+        events: {
+            "click a.btn": "navigate"
+        },
+
+        navigate: function (event) {
+            var chosen = $(event.target).siblings("select").val();
+            this.__navigate(event, '/ready/'+chosen);
+        }
+    });
+
+    var TopicsView = PageView.extend({
+        template: Handlebars.compile($("#template-topics").html())
+
+    });
+
+    var ReadyView = PageView.extend({
+        template: Handlebars.compile($("#template-ready").html())
+    });
+
+    var WaitingView = PageView.extend({
+        template: Handlebars.compile($("#template-waiting").html())
+    });
+
+    var HandshakeView = PageView.extend({
+        template: Handlebars.compile($("#template-handshake").html())
+    });
+
+    var MeltView = PageView.extend({
+        template: Handlebars.compile($("#template-melt").html())
+    });
+
+    var ThanksView = PageView.extend({
+        template: Handlebars.compile($("#template-thanks").html())
+    });
+
+
+    var View = Backbone.View.extend({
+
+        pages: {"events": EventsView,
+                "topics": TopicsView,
+                "ready": ReadyView,
+                "waiting": WaitingView,
+                "handshake": HandshakeView, 
+                "melt": MeltView,
+                "thanks": ThanksView
+               },
+
+        initialize: function () {
+            _.keys(this.pages).map(function (route) {
+                this.options.router.on("route:"+route,
+                                       function (event_id, person_id) {
+                                           this.render(route, arguments);
+                                       }, this);
+            }, this);
+        },
+
+        render: function (route, ids) {
+            var view = new this.pages[route]({el: this.$el,
+                                              router: this.options.router,
+                                              event_id: ids[0],
+                                              person_id: ids[1]});
+
+            view.render();
+        }
+
+    });
 
     var router = new Router(),
         view = new View({router: router,
