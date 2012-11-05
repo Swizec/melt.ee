804efd736e82317d55add37bf4f1a95acb6d8119
Swizec Teller <swizec@swizec.com>
2012-11-05 15:16:37 +0100

    disable app until launch

--- routes/mobile.js
+++ routes/mobile.js
@@ -8,11 +8,17 @@ exports.index = function (req, res) {
     // see if user needs to fill in topics
     models.linkedin_users.findOne({linkedin_id: req.session.user_sess.id},
         function (err, result) {
+            // disable everything until launch
+            if (new Date() < new Date(2012, 11, 10, 15, 00)
+                && settings.dev_ips.indexOf(req.ip) < 0){
+                return res.render('mobile-disabled.jade', req.session.user_sess);
+            }
+
             if (result.topic1+result.topic2+result.topic3 === ""
                 && !req.path.match(/\/topics/)) {
                 return res.redirect('/mobile/topics');
             }
-            
+                    
             res.render('mobile.jade', req.session.user_sess);
         });
 };
