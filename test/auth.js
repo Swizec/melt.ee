1e4e85f29560d21efca316fd08dfd44b43327f72
Swizec Teller <swizec@swizec.com>
2012-11-06 18:10:36 +0100

    separate tests for auth

---
+++ test/auth.js
@@ -0,0 +1,51 @@
+
+var chai = require('chai'),
+    mocha = require('mocha'),
+    should = chai.should();
+
+var request = require('supertest'),
+    io = require('socket.io-client');
+
+var app = require('../app').app,
+    settings = require('../settings');
+
+
+describe('Auth', function () {
+    
+    it('redirects to login', function (done) {
+        request(app)
+            .get('/mobile/')
+            .expect(302)
+            .end(function (err, res) {
+                res.headers.location.should.equal('/login');
+                done();
+            });
+    });
+
+    it('starts oauth', function (done) {
+        request(app)
+            .get('/auth')
+            .expect(302)
+            .end(function (err, res) {
+                res.header.location.should.include('https://www.linkedin.com/uas/oauth/authenticate');
+                done();
+            });
+    });
+
+    it('stubs session', function (done) {
+        var API = db.model('linkedin_users', new mongoose.Schema());
+        API.find({}).execFind(function (err, result) {
+            request(app)
+                .get('/__stub_session')
+                .set('X-User-Id', result[0].linkedin_id)
+                .expect(200)
+                .end(function (err, res) {
+                    request(app)
+                        .get('/mobile/')
+                        .set('cookie', res.header['set-cookie'])
+                        .expect(200, done);
+                });
+        });
+    });
+
+});
--- test/mobile.js
+++ test/mobile.js
@@ -11,42 +11,6 @@ var app = require('../app').app,
 
 describe('Mobile site', function () {
 
-    it('redirects to login', function (done) {
-        request(app)
-            .get('/mobile/')
-            .expect(302)
-            .end(function (err, res) {
-                res.headers.location.should.equal('/login');
-                done();
-            });
-    });
-
-    it('starts oauth', function (done) {
-        request(app)
-            .get('/auth')
-            .expect(302)
-            .end(function (err, res) {
-                res.header.location.should.include('https://www.linkedin.com/uas/oauth/authenticate');
-                done();
-            });
-    });
-
-    it('stubs session', function (done) {
-        var API = db.model('linkedin_users', new mongoose.Schema());
-        API.find({}).execFind(function (err, result) {
-            request(app)
-                .get('/__stub_session')
-                .set('X-User-Id', result[0].linkedin_id)
-                .expect(200)
-                .end(function (err, res) {
-                    request(app)
-                        .get('/mobile/')
-                        .set('cookie', res.header['set-cookie'])
-                        .expect(200, done);
-                });
-        });
-    });
-
     it('redirects to topics when fresh user', function (done) {
         stub_session(function (cookie) {
             models.linkedin_users.find({}, function (err, result) {
